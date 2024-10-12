aef2433f46fde35536ef1ee199fdf9c552976deb
Saad-Momin00313 <saadmomin00313@gmail.com>
2024-10-12 18:26:00 +0530

    Updated project files

--- scripts/details.js
+++ scripts/details.js
@@ -60,7 +60,7 @@ async function fetchRecommendations(movieId) {
         const data = await response.json();
         
         if (data.results && data.results.length > 0) {
-            displayRecommendations(data.results.slice(0, 4));
+            displayRecommendations(data.results.slice(0, 12));
         } else {
             recoContainer.innerHTML = '<p class="text-xl">No recommendations available.</p>';
         }
